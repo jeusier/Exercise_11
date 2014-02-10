6ef28b217c4ffc8e6cac0487576afbfaf094a157
Jason Piros <jahsan87@gmail.com>
2014-02-10 10:33:18 -1000

    completed Exercise 11

--- js/ex11.js
+++ js/ex11.js
@@ -9,48 +9,65 @@ $(document).ready(function(){
 		$("#tabs").tabs();
 	});
 
-	$("#ex11_container").append("<button></button>");
+	$("#ex11_container").append("<button id='contactus'></button>");
 
+	$(function(){
+		$("#contactus").button({icons: { primary: "ui-icon-mail-closed"}});
+		$("#contactus").button({ label: "Contact Us" });
+	});
 
-	$("button").button({icons: { primary: "ui-icon-mail-closed"}});
-	$("button").button({ label: "Contact Us" });
-			$("#slider").slider({
-				max: 100,
-				min: 0,
-				value: 25,
-				slide: function(e,ui){
-					$("#currentval").html(ui.value);
-				}
-			});
-			$("#datepicker").datepicker({
-				changeMonth: true,
-				changeYear: true,
-				showOtherMonths: true,
-				selectOtherMonths: true
-			});
-
-
-
-
-		$("#dialog").dialog({ autoOpen: false}).dialog({modal: true}).dialog({minWidth: 400});
-		$("button").click(function(){
-		
-		$("#dialog").dialog("open");
-
-		$(document).tooltip();
-
-
-
-
+	$(function(){
+		var devNames = ["Jason S", "Jason P", "Derek A", "Kingtak W", "Ray F", "Tyler B", "Jon B"," Kelli B"];
+		$("#devnames").autocomplete({
+			source: devNames
+		});
 
 	});
 
 
 
+	$(function() {
+		$( "#datepicker" ).datepicker({
+			changeMonth: true,
+			changeYear: true,
+			showOtherMonths: true,
+			selectOtherMonths: true
+		});
+		$( "#anim" ).change(function() {
+		$( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
+		});
+	});
 
+
+
+	$(function(){
+		$("#dialog").dialog({ autoOpen: false}).dialog({modal: true}).dialog({minWidth: 400});
+		$("button").click(function(){
+			$("#dialog").dialog("open");
+			$(document).tooltip();
+		});
+	});
+
+
+	 $(function() {
+		$( "#slider" ).slider({
+		value:25,
+		min: 0,
+		max: 100,
+		step: 1,
+		slide: function( event, ui ) {
+		$( "#amount" ).val(ui.value );
+		}
+		});
+		$( "#amount" ).val($( "#slider" ).slider( "value" ) );
+	});
 
 	
 });
+
+	function submitFunction(){
+		document.getElementById("myForm").submit();
+	}
 
 
    
